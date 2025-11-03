6079a6ac0070141ba0ae26d5028779d1b4d96391
Mohammad-Esmaeilirad-mesmaeil_bestbuy <mesmaeil@bestbuycanada.ca>
2025-11-03 13:01:15 -0800

    Add memoization to fib

--- src/bby/algorithms/dynamic/programming/Fibonacci.java
+++ src/bby/algorithms/dynamic/programming/Fibonacci.java
@@ -1,10 +1,19 @@
 package bby.algorithms.dynamic.programming;
 
+import java.util.HashMap;
+
 public class Fibonacci {
 
     public static int fib(int n) {
         if (n <= 2) { return 1;}
         return fib(n -1) + fib(n -2);
+    }
+
+    public static int fibMemoized(int n, HashMap<Integer, Integer> memo) {
+        if (memo.containsKey(n)) { return memo.get(n); }
+        if (n <= 2) { return 1;}
+        memo.put(n, fibMemoized(n -1, memo) + fibMemoized(n -2, memo));
+        return memo.get(n);
     }
 
 }
