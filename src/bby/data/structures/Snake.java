d352ff63c42beac5d1c5a990932acd11afbc9f52
Mohammad-Esmaeilirad-mesmaeil_bestbuy <mesmaeil@bestbuycanada.ca>
2026-02-20 23:20:28 -0800

    create initial snake drawing grid

--- src/bby/data/structures/Snake.java
+++ src/bby/data/structures/Snake.java
@@ -1,4 +1,39 @@
 package bby.data.structures;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Snake {
+
+    List<List<Integer>> snakeBody;
+    public Snake() {
+        this.snakeBody = new ArrayList<>(Arrays.asList(
+                Arrays.asList(4,1),
+                Arrays.asList(4,2),
+                Arrays.asList(4,3),
+                Arrays.asList(4,4)
+        ));
+    }
+
+    public void draw() {
+        List<List<String>> grid = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            List<String> row = new ArrayList<>();
+            for (int j = 0; j < 10; j++) {
+                row.add(" ");
+            }
+            grid.add(row);
+        }
+
+        grid.forEach(row -> System.out.println(String.join("|", row)));
+    }
+
+    public static void main(String[] args) {
+        Snake larry = new Snake();
+        larry.draw();
+    }
+
+
+
 }
