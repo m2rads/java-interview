42a342a060fe6b254c4fcf52723f7c5049e28f7b
Mohammad-Esmaeilirad-mesmaeil_bestbuy <mesmaeil@bestbuycanada.ca>
2026-02-21 00:33:15 -0800

    add play mode

--- src/bby/data/structures/Snake.java
+++ src/bby/data/structures/Snake.java
@@ -1,5 +1,8 @@
 package bby.data.structures;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -8,6 +11,8 @@ import java.util.List;
 public class Snake {
 
     List<List<Integer>> snakeBody;
+    boolean gameRunning;
+
     public Snake() {
         this.snakeBody = new ArrayList<>(Arrays.asList(
                 Arrays.asList(4,1),
@@ -15,9 +20,13 @@ public class Snake {
                 Arrays.asList(4,3),
                 Arrays.asList(4,4)
         ));
+
+        this.gameRunning = true;
     }
 
     public void draw() {
+        this.clearScreen();
+
         List<List<String>> grid = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             List<String> row = new ArrayList<>();
@@ -60,14 +69,62 @@ public class Snake {
 
     }
 
-    public static void main(String[] args) {
-        Snake larry = new Snake();
-        larry.draw();
+    private void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
-        larry.move("UP");
-        larry.draw();
-        larry.move("RIGHT");
-        larry.draw();
+    public void play() throws IOException, InterruptedException {
+        // Set up terminal for raw mode (Unix/Mac/Linux)
+        String[] cmd = {"/bin/sh", "-c", "stty raw -echo < /dev/tty"};
+        Runtime.getRuntime().exec(cmd).waitFor();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Use WASD to move. Press Q to quit.");
+        Thread.sleep(2000);
+
+        new Thread(() -> {
+            try {
+                while (gameRunning) {
+                    draw();
+                    Thread.sleep(100);  // Frame rate
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        while (gameRunning) {
+            if (reader.ready()) {
+                char keypress = (char) reader.read();
+
+                if (keypress == 'w') this.move("UP");
+                else if (keypress == 'a') this.move("LEFT");
+                else if (keypress == 's') this.move("DOWN");
+                else if (keypress == 'd') this.move("RIGHT");
+                else if (keypress == 'q') {
+                    gameRunning = false;
+                    break;
+                }
+            }
+        }
+
+        // Restore terminal
+        String[] restore = {"/bin/sh", "-c", "stty sane < /dev/tty"};
+        Runtime.getRuntime().exec(restore).waitFor();
+
+        System.out.println("\nGame Over!");
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            Snake larry = new Snake();
+            larry.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
